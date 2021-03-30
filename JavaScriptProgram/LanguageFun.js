1d95057b0aee76a4e0e3752acc2b13bcb95fe1ea
Rinu <alphonsrinu93@gmail.com>
2021-03-30 22:51:30 +0530

    committing files

---
+++ JavaScriptProgram/LanguageFun.js
@@ -0,0 +1,17 @@
+// identifiers?
+//
+
+
+
+
+
+
+// variables used for represting m/m location
+//var, let ,const(to declaring js)
+// var (using for global rep)
+var name="Rinu";
+var age=27;
+var company="Luminar";
+var location="Kakkanad";
+console.log("Hai am " + name + age + " years old")
+console.log(`Our company ${company} is located in ${location}`);
